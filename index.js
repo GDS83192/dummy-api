d3303747b348f10694971ea8cd7432cb9a483970
GDS83192 <graham.squires3@gmail.com>
2023-07-14 00:11:57 -0400

    fix

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.listen(process.env.PORT || 3000, () => console.log('server running'));
+app.listen(process.env.PORT || 3000, () => console.log("server running"));
 
-app.get('/', (req, res) => {
-    res.send('Api is running.')
-    })
+app.get("/", (req, res) => {
+  res.send("Api is running.");
+});
 
-const posts = require('./posts')
+const posts = require("./posts");
 
-app.get('/posts', (req, res) => {
-    res.json(posts)
-})
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
