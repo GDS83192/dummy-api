db773fd550b1890bbeabfe1e7e0740c51112c8fe
GDS83192 <graham.squires3@gmail.com>
2023-07-13 23:59:19 -0400

    first commit

---
+++ posts.js
@@ -0,0 +1,184 @@
+module.exports = [
+    {
+      "id": "64b0c5665f6044efd2011b42",
+      "email": "leslie_lopez@concility.lu",
+      "roles": [
+        "member",
+        "owner"
+      ],
+      "apiKey": "a8df61e5-54fb-41db-9bde-b1e31245a00c",
+      "profile": {
+        "dob": "1994-03-12",
+        "name": "Leslie Lopez",
+        "about": "Labore deserunt commodo labore dolore cillum exercitation culpa nostrud nostrud labore tempor proident ipsum. Consectetur nostrud anim labore Lorem est aute dolore irure dolore sunt id mollit laboris culpa.",
+        "address": "94 Hegeman Avenue, Darbydale, Missouri",
+        "company": "Concility",
+        "location": {
+          "lat": -11.472181,
+          "long": 83.215931
+        }
+      },
+      "username": "leslie94",
+      "createdAt": "2014-09-19T11:00:00.055Z",
+      "updatedAt": "2014-09-20T11:00:00.055Z"
+    },
+    {
+      "id": "64b0c566211c47a8be38072c",
+      "email": "eve_marshall@centree.lk",
+      "roles": [
+        "guest",
+        "admin"
+      ],
+      "apiKey": "a3918fa8-9f0f-4434-bff5-e2b348ff2d27",
+      "profile": {
+        "dob": "1993-03-30",
+        "name": "Eve Marshall",
+        "about": "Culpa sunt minim velit Lorem non eiusmod exercitation dolore amet do magna ipsum excepteur consequat. Mollit anim quis qui dolore esse veniam labore sunt incididunt.",
+        "address": "80 Royce Street, Cazadero, Florida",
+        "company": "Centree",
+        "location": {
+          "lat": -42.820441,
+          "long": 89.513657
+        }
+      },
+      "username": "eve93",
+      "createdAt": "2014-10-04T20:23:02.135Z",
+      "updatedAt": "2014-10-05T20:23:02.135Z"
+    },
+    {
+      "id": "64b0c56628e9f8837232ff1d",
+      "email": "elsa_baird@firewax.mu",
+      "roles": [
+        "admin",
+        "guest"
+      ],
+      "apiKey": "dcebf0bc-23bd-4543-97c9-5c725aef2e74",
+      "profile": {
+        "dob": "1994-05-29",
+        "name": "Elsa Baird",
+        "about": "Duis proident sit ullamco mollit deserunt. Eiusmod do incididunt eiusmod velit anim eiusmod excepteur reprehenderit consectetur eiusmod incididunt ad excepteur.",
+        "address": "52 Underhill Avenue, Turah, Massachusetts",
+        "company": "Firewax",
+        "location": {
+          "lat": 78.481836,
+          "long": -166.172305
+        }
+      },
+      "username": "elsa94",
+      "createdAt": "2013-04-02T07:11:56.217Z",
+      "updatedAt": "2013-04-03T07:11:56.217Z"
+    },
+    {
+      "id": "64b0c566a0d24fcbc9cb3581",
+      "email": "pate_bruce@isotrack.prof",
+      "roles": [
+        "owner",
+        "admin"
+      ],
+      "apiKey": "93d5acc7-621d-4d20-b713-053eed755d9c",
+      "profile": {
+        "dob": "1991-12-05",
+        "name": "Pate Bruce",
+        "about": "Duis laborum amet dolor velit. Irure culpa labore aliqua Lorem pariatur non proident dolore.",
+        "address": "82 Bushwick Court, Tuttle, District Of Columbia",
+        "company": "Isotrack",
+        "location": {
+          "lat": 8.234581,
+          "long": -132.542561
+        }
+      },
+      "username": "pate91",
+      "createdAt": "2011-05-19T20:02:23.113Z",
+      "updatedAt": "2011-05-20T20:02:23.113Z"
+    },
+    {
+      "id": "64b0c566bbcf6d9030a9ccf8",
+      "email": "juliette_foreman@uniworld.irish",
+      "roles": [
+        "admin"
+      ],
+      "apiKey": "4672fab0-9d95-445a-af59-acf82c747aca",
+      "profile": {
+        "dob": "1989-09-08",
+        "name": "Juliette Foreman",
+        "about": "Consequat exercitation est dolor id eu. Culpa ullamco dolore aliqua laborum labore ad non minim quis culpa aliquip.",
+        "address": "70 Laurel Avenue, Brecon, Idaho",
+        "company": "Uniworld",
+        "location": {
+          "lat": -2.132614,
+          "long": -55.055395
+        }
+      },
+      "username": "juliette89",
+      "createdAt": "2010-10-18T09:48:52.052Z",
+      "updatedAt": "2010-10-19T09:48:52.052Z"
+    },
+    {
+      "id": "64b0c566227fda03c6da99f2",
+      "email": "barbara_velasquez@eschoir.sb",
+      "roles": [
+        "guest",
+        "owner"
+      ],
+      "apiKey": "985f0544-a0f8-4eee-929d-e86913322e1c",
+      "profile": {
+        "dob": "1990-12-21",
+        "name": "Barbara Velasquez",
+        "about": "Proident quis amet mollit exercitation dolore ea magna tempor. Ad incididunt cupidatat culpa officia.",
+        "address": "67 Coleridge Street, Motley, Louisiana",
+        "company": "Eschoir",
+        "location": {
+          "lat": -31.153428,
+          "long": 163.746986
+        }
+      },
+      "username": "barbara90",
+      "createdAt": "2011-05-30T07:36:28.651Z",
+      "updatedAt": "2011-05-31T07:36:28.651Z"
+    },
+    {
+      "id": "64b0c566566141be1ecf5c3c",
+      "email": "nadine_todd@bunga.property",
+      "roles": [
+        "member"
+      ],
+      "apiKey": "34d89867-8792-4b2b-b132-3fb03220ec98",
+      "profile": {
+        "dob": "1994-05-12",
+        "name": "Nadine Todd",
+        "about": "Labore cillum magna deserunt laboris cillum veniam voluptate. Velit eu incididunt commodo eiusmod sit nisi qui aliqua in cillum adipisicing.",
+        "address": "28 Gerald Court, Wikieup, Georgia",
+        "company": "Bunga",
+        "location": {
+          "lat": 13.741193,
+          "long": 119.216136
+        }
+      },
+      "username": "nadine94",
+      "createdAt": "2012-11-30T04:56:42.337Z",
+      "updatedAt": "2012-12-01T04:56:42.337Z"
+    },
+    {
+      "id": "64b0c566f7c3ad219129bb0b",
+      "email": "witt_lott@geekmosis.ne",
+      "roles": [
+        "guest",
+        "member"
+      ],
+      "apiKey": "2ada87a6-9894-4eea-97a9-720b195384b4",
+      "profile": {
+        "dob": "1992-09-25",
+        "name": "Witt Lott",
+        "about": "Occaecat ex tempor sit aliquip aliquip reprehenderit proident mollit non. Fugiat sit ullamco adipisicing tempor et sint et deserunt anim id laborum excepteur labore.",
+        "address": "2 Nostrand Avenue, Blende, Delaware",
+        "company": "Geekmosis",
+        "location": {
+          "lat": 58.045738,
+          "long": -134.074721
+        }
+      },
+      "username": "witt92",
+      "createdAt": "2012-11-11T13:29:32.819Z",
+      "updatedAt": "2012-11-12T13:29:32.819Z"
+    }
+  ]
